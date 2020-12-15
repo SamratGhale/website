aa871f14d000b7faefade7b0db3acbf397c76248
SamratGhale <samrat.biz19@gmail.com>
2020-12-16 01:06:12 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [repos, setRepos] = useState([{}]);
+  const api = "https://api.github.com/users/SamratGhale/repos";
+  useEffect(() => {
+    setRepos([{}]);
+    fetch(api)
+      .then((x) => x.json())
+      .then((y) => setRepos(y));
+  }, [api]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <a
+        style={{ color: "black", fontSize: "larger" }}
+        href="http://github.com/SamratGhale"
+      >
+        Samrat Ghale
+      </a>
+      <p>bio: student, </p>
+      <BrowserRouter>
+        <div className="center-screen">
+          links:
+          <a href="http://twitter.com/samrat_ghale">@twitter</a>
+          <a href="malito: samrat.biz19@gmail.com">@gmail</a>
+          <Link to="/blog">@blog</Link>
+        </div>
+        <div className="center-screen">
+          repos:
+          <br />
+          {repos.map((item, idx) => {
+            return (
+              <div>
+                <a href={item.html_url}>{item.name}</a>
+                <br />
+                <br />
+              </div>
+            );
+          })}
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
